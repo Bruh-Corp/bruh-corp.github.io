8307bce42f2725c78d17b62827e6eb43c71a09b3
AbbanFahim <lloydtechno58@gmail.com>
2023-06-03 14:54:19 +0400

    final refactor

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ toggleButton.addEventListener("click", function () {
   tri.classList.toggle("rotate");
 });
 
-const triContact = document.getElementById("triContact");
-const contactButton = document.getElementById("contactButton");
-const contactBox = document.getElementById("contact");
-contactButton.addEventListener("click", function () {
-  contactBox.classList.toggle("abc");
-  triContact.classList.toggle("rotate");
-});
+// const triContact = document.getElementById("triContact");
+// const contactButton = document.getElementById("contactButton");
+// const contactBox = document.getElementById("contact");
+// contactButton.addEventListener("click", function () {
+//   contactBox.classList.toggle("abc");
+//   triContact.classList.toggle("rotate");
+// });
 
 const navbar = document.querySelector("nav");
 const navList = document.getElementById("nav-list");
