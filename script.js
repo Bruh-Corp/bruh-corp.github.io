dc928fa61c317fd561449ec92585bb511973a717
Talha-Ijaz-Qureshi <mrwcggr121@gmail.com>
2023-12-09 23:17:55 +0400

    upda

--- script.js
+++ script.js
@@ -39,17 +39,34 @@ document.addEventListener('click', function(event) {
   }
 });
 
-
-
 const navbar = document.querySelector("nav");
 const navList = document.getElementById("nav-list");
 const arrow = document.getElementById("arrow");
+let isNav = false;
 arrow.addEventListener("click", function () {
   navList.classList.toggle("visible");
   arrow.classList.toggle("rotate");
+  isNav = navList.classList.contains("visible");
+  console.log(isNav)
 });
 
 function playSound(fileName) {
   var snd = new Audio(fileName);
   snd.play();
 }
+
+
+document.addEventListener("click", function(event) {
+  const toggleBtn = document.getElementById("arrow");
+  const navbar = document.getElementById("nav-list");
+  const target = event.target;
+  const isNavbar = target === navbar || navbar.contains(target);
+  const isToggleBtn = target === toggleBtn;
+  console.log("state: ", isNav)
+  if (!isNavbar && !isToggleBtn && isNav == true) {
+    navbar.classList.remove("visible");
+    toggleBtn.classList.toggle("rotate")
+    isNav = false
+    console.log("state: ", isNav)
+  }
+});
